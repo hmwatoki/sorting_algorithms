32376196f746cbdb135a8d8612def13a1d31c523
hmwatoki <hmwatoki@gmail.com>
2023-06-27 19:53:26 +0300

    fix: Add sort.h and update shell sort algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "sort.h"
 /**
  * shell_sort - Sorts an array of integers in ascending order
  * @array: The array to be sorted
@@ -6,12 +7,13 @@
  */
 void shell_sort(int *array, size_t size)
 {
-size_t space = 1, j, i;
+size_t space = 1, i, j;
 int tmp;
 while (space < size / 3)
 space = space * 3 + 1;
 while (space > 0)
 {
+printf("Array at interval %lu: ", space);
 for (i = space; i < size; i++)
 {
 tmp = array[i];
@@ -19,9 +21,7 @@ for (j = i; j >= space && array[j - space] > tmp; j -= space)
 array[j] = array[j - space];
 array[j] = tmp;
 }
-space = (space - 1) / 3;
-printf("Array after decreasing the interval by %lu: ", space);
-for (i = 0; i < size; i++)
-printf("%d%s", array[i], i == size - 1 ? "\n" : ", ");
+print_array(array, size);
+space /= 3;
 }
 }
